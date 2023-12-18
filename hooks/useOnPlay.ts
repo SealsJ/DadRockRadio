4296cdf49f498111303dcf9875790632c33415c7
Jeremy ✔️ <jeremyseals@utexas.edu>
2023-12-18 17:04:33 -0600

    Refactor(project): First Upload to Github

---
+++ hooks/useOnPlay.ts
@@ -0,0 +1,30 @@
+import SubscribeModal from "@/components/SubscribeModal";
+import { Song } from "@/types";
+import useAuthModal from "./useAuthModal";
+import usePlayer from "./usePlayer";
+import useSubscribeModal from "./useSubscribeModal";
+import { useUser } from "./useUser";
+
+const useOnPlay = (songs: Song[]) => {
+  const player = usePlayer();
+  const subscribeModal = useSubscribeModal();
+  const authModal = useAuthModal();
+  const { user, subscription } = useUser();
+
+  const onPlay = (id: string) => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    if(!subscription) {
+      return subscribeModal.onOpen();
+    }
+
+    player.setId(id);
+    player.setIds(songs.map((song) => song.id));
+  };
+
+  return onPlay;
+};
+
+export default useOnPlay;
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "ogudsmiwbckypktocgaz.supabase.co"
+    ]
+  }
+}
+
+module.exports = nextConfig
